f82a1c3d3f8e1981c8913af6317e287de2727a33
unknown <gideonanokyebrown@gmail.com>
2025-05-30 05:31:56 +0000

    third  commit

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white py-6 mt-auto">
+    <footer className="bg-gray-900 text-white py-6  mt-auto">
       <div className="max-w-7xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
         <p className="text-sm text-center md:text-left">
           &copy; {new Date().getFullYear()} Rango Stock Inventory App. Created by Gideon Anokye (Brown.Dev) All rights reserved.
