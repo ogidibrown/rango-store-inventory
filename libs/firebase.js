ab82b3500aa85568f7f99a74768452b62c4fb41f
ogidibrown <gideonanokyebrown@gmail.com>
2025-06-25 14:40:14 +0000

    Update firebase.js
    updated firebase config

--- libs/firebase.js
+++ libs/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDeVTjtr0QNBxrQoGGOI4YeUKBTfLhhg-I",
-    authDomain: "store-inventory-d0806.firebaseapp.com",
-    projectId: "store-inventory-d0806",
-    storageBucket: "store-inventory-d0806.firebasestorage.app",
-    messagingSenderId: "656561678398",
-    appId: "1:656561678398:web:4941495dbbda87ce90096f",
-    measurementId: "G-7FGTSL1S4S"
+  apiKey: "AIzaSyDeVTjtr0QNBxrQoGGOI4YeUKBTfLhhg-I",
+  authDomain: "store-inventory-d0806.firebaseapp.com",
+  projectId: "store-inventory-d0806",
+  storageBucket: "store-inventory-d0806.firebasestorage.app",
+  messagingSenderId: "656561678398",
+  appId: "1:656561678398:web:4941495dbbda87ce90096f",
+  measurementId: "G-7FGTSL1S4S"
 };
 
 const app = initializeApp(firebaseConfig);
